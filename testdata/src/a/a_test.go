91ae5fc164d81e84c8b266e9f79f7ce2f3e881eb
favonia <favonia@gmail.com>
2022-08-01 06:11:56 -0500

    test: add test cases for ExampleXXX

--- testdata/src/a/a_test.go
+++ testdata/src/a/a_test.go
@@ -10,6 +10,10 @@ func BenchmarkX(b *testing.B)   {}
 func Benchmark_x(b *testing.B)  {}
 func BenchmarkX_x(b *testing.B) {}
 
+func ExampleX(f *testing.F)   {}
+func Example_x(f *testing.F)  {}
+func ExampleX_x(f *testing.F) {}
+
 func FuzzX(f *testing.F)   {}
 func Fuzz_x(f *testing.F)  {}
 func FuzzX_x(f *testing.F) {}
@@ -21,6 +25,10 @@ func xTestX_x(t *testing.T) {} // want "xTestX_x contains underscore. You should
 func xBenchmarkX(b *testing.B)   {}
 func xBenchmark_x(b *testing.B)  {} // want "xBenchmark_x contains underscore. You should use mixedCap or MixedCap."
 func xBenchmarkX_x(b *testing.B) {} // want "xBenchmarkX_x contains underscore. You should use mixedCap or MixedCap."
+
+func xExampleX(f *testing.F)   {}
+func xExample_x(f *testing.F)  {} // want "xExample_x contains underscore. You should use mixedCap or MixedCap."
+func xExampleX_x(f *testing.F) {} // want "xExampleX_x contains underscore. You should use mixedCap or MixedCap."
 
 func xFuzzX(f *testing.F)   {}
 func xFuzz_x(f *testing.F)  {} // want "xFuzz_x contains underscore. You should use mixedCap or MixedCap."
